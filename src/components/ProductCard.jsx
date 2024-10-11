e81b98a795461a24570eeee8cc27e7d7137423f6
Rohit <Rohitsmali9421@gmail.com>
2024-10-11 21:17:43 +0530

    .

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({imageURL3d,imgaeUrl ,title}) => {
 
   // Handle the AR button click to activate AR mode
   const handleARClick = () => {
     const modelViewer = document.getElementById('ar-model-viewer');
-    modelViewer.setAttribute('src', 'https://res.cloudinary.com/dhturqqs5/image/upload/v1728660727/SIT%20Project/erwon0cp3lyka6raff6u.glb');
+    modelViewer.setAttribute('src', {imageURL3d});
     modelViewer.activateAR(); // Trigger AR mode
   };
 
@@ -14,12 +14,12 @@ const ProductCard = () => {
       {/* Product Image */}
       <img
         className="product-image mb-4 rounded-lg object-contain h-48 w-full"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNHL0-PgalMFn9qZjqiI_fB5GOAFv2FQHT9g&s" 
+        src={imgaeUrl} 
         alt="LED Screen Placeholder"
       />
 
       {/* Product Title */}
-      <h2 className="text-2xl font-semibold text-gray-800 mb-2 text-center">LED Screen</h2>
+      <h2 className="text-2xl font-semibold text-gray-800 mb-2 text-center">{title}</h2>
 
       {/* AR Button */}
       <button
--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -10,10 +10,10 @@ const ProductGrid = () => {
       {/* Responsive Grid for Product Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {/* Render multiple ProductCard components */}
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard imageURL3d="https://res.cloudinary.com/dhturqqs5/image/upload/v1728661338/SIT%20Project/irpooy3sv0urgtsd3rzj.glb " imgaeUrl=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA6u1bqzVxGo9uMh83YAB8woS5ItaSh7DA5A&s" title="Chair " />
+        <ProductCard imageURL3d="https://res.cloudinary.com/dhturqqs5/image/upload/v1728661338/SIT%20Project/irpooy3sv0urgtsd3rzj.glb " imgaeUrl=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA6u1bqzVxGo9uMh83YAB8woS5ItaSh7DA5A&s" title="Chair " />
+        <ProductCard imageURL3d="https://res.cloudinary.com/dhturqqs5/image/upload/v1728661338/SIT%20Project/irpooy3sv0urgtsd3rzj.glb " imgaeUrl=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA6u1bqzVxGo9uMh83YAB8woS5ItaSh7DA5A&s" title="Chair " />
+        
         {/* Add more ProductCard components as needed */}
       </div>
     </div>
