2b24c00a7bacfa6dfe9b703c1eba518ec6f554fc
Rohit <Rohitsmali9421@gmail.com>
2024-10-11 21:32:18 +0530

    .

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const ProductCard = ({ imageURL3d, imageUrl, title }) => {
 
-  // Handle the AR button click to activate AR mode
   const handleARClick = () => {
     const modelViewer = document.getElementById('ar-model-viewer');
     modelViewer.setAttribute('src', imageURL3d);
@@ -11,17 +10,13 @@ const ProductCard = ({ imageURL3d, imageUrl, title }) => {
 
   return (
     <div className="product-card bg-white rounded-lg shadow-md p-4 text-center">
-      {/* Product Image */}
       <img
         className="product-image mb-4 rounded-md object-cover h-48 w-full"
         src={imageUrl}
         alt={`${title} Placeholder`}
       />
-
-      {/* Product Title */}
       <h2 className="text-2xl font-semibold text-gray-800 mb-2">{title}</h2>
 
-      {/* AR Button */}
       <button
         className="ar-button bg-blue-500 text-white px-4 py-2 mt-4 rounded hover:bg-blue-600 transition duration-300 ease-in-out"
         onClick={handleARClick}
@@ -29,7 +24,6 @@ const ProductCard = ({ imageURL3d, imageUrl, title }) => {
         View in AR
       </button>
 
-      {/* Hidden AR model viewer for full-screen AR mode */}
       <model-viewer
         id="ar-model-viewer"
         ar
--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -6,16 +6,13 @@ const ProductGrid = () => {
     <div className="product-grid-container max-w-7xl mx-auto px-4 py-8">
       <h2 className="text-3xl font-bold text-center mb-8">Our Products</h2>
 
-      {/* Responsive Grid for Product Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {/* Render multiple ProductCard components */}
         <ProductCard
           imageURL3d="https://res.cloudinary.com/dhturqqs5/image/upload/v1728660727/SIT%20Project/erwon0cp3lyka6raff6u.glb"
           imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA6u1bqzVxGo9uMh83YAB8woS5ItaSh7DA5A&s"
           title="Chair"
         />
 
-        {/* Add more ProductCard components as needed */}
         <ProductCard
           imageURL3d="https://res.cloudinary.com/dhturqqs5/image/upload/v1728660727/SIT%20Project/erwon0cp3lyka6raff6u.glb"
           imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqF5EQzEorVdBLcz2LScQCoRH6PQ&s"
